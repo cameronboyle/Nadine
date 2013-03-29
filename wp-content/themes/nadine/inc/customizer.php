c5131d8749fe8b7dad6a89972c97f4f99121bcb3
Cameron Boyle <cameronboyle@mac.com>
2013-03-29 18:12:00 +1100

    _s and WordPress install
    Fresh install of _s and WordPress

---
+++ wp-content/themes/nadine/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Nadine Theme Customizer
+ *
+ * @package Nadine
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function nadine_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'nadine_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function nadine_customize_preview_js() {
+	wp_enqueue_script( 'nadine_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130304', true );
+}
+add_action( 'customize_preview_init', 'nadine_customize_preview_js' );
