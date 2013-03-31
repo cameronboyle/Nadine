52bb764a77f13a1a685e80923e86f0c117b77c40
Cameron Boyle <cameronboyle@mac.com>
2013-03-31 17:43:55 +1100

    New Child theme

--- wp-content/themes/nadine/functions.php
+++ wp-content/themes/nadine/functions.php
@@ -1,146 +1,60 @@
 <?php
-/**
- * Nadine functions and definitions
- *
- * @package Nadine
- */
+	wp_enqueue_script('jquery');
 
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+	wp_enqueue_script( 'grayscale',get_bloginfo( "stylesheet_directory")."/js/grayscale.js",array('jquery'),time());
 
-/*
- * Load Jetpack compatibility file.
- */
-require( get_template_directory() . '/inc/jetpack.php' );
+	add_action('init', 'project_custom_init');  
 
-if ( ! function_exists( 'nadine_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which runs
- * before the init hook. The init hook is too late for some features, such as indicating
- * support post thumbnails.
- */
-function nadine_setup() {
+	// Registering Menus
+	function register_menu() { register_nav_menu('navigation', __('Main Navigation')); }
+	add_action('init', 'register_menu');
+	
+	// Register our Scripts
+	function register_js() {
+		if (!is_admin()) {
+			wp_deregister_script('jquery');
+			wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+			wp_register_script('quicksand', get_stylesheet_directory_uri() . '/js/jquery.quicksand.js', 'jquery');
+			wp_register_script('easing', get_stylesheet_directory_uri() . '/js/jquery.easing.1.3.js', 'jquery');
+			wp_register_script('custom', get_stylesheet_directory_uri() . '/js/jquery.custom.js', 'jquery', '1.0', TRUE);
+			wp_register_script('prettyPhoto', get_stylesheet_directory_uri() . '/js/jquery.prettyPhoto.js', 'jquery');
 
-	/**
-	 * Custom template tags for this theme.
-	 */
-	require( get_template_directory() . '/inc/template-tags.php' );
 
-	/**
-	 * Custom functions that act independently of the theme templates
-	 */
-	require( get_template_directory() . '/inc/extras.php' );
-
-	/**
-	 * Customizer additions
-	 */
-	require( get_template_directory() . '/inc/customizer.php' );
-
-	/**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on Nadine, use a find and replace
-	 * to change 'nadine' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'nadine', get_template_directory() . '/languages' );
-
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
-
-	/**
-	 * Enable support for Post Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
-
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'nadine' ),
-	) );
-
-	/**
-	 * Enable support for Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
-}
-endif; // nadine_setup
-add_action( 'after_setup_theme', 'nadine_setup' );
-
-/**
- * Setup the WordPress core custom background feature.
- *
- * Use add_theme_support to register support for WordPress 3.4+
- * as well as provide backward compatibility for WordPress 3.3
- * using feature detection of wp_get_theme() which was introduced
- * in WordPress 3.4.
- *
- * @todo Remove the 3.3 support when WordPress 3.6 is released.
- *
- * Hooks into the after_setup_theme action.
- */
-function nadine_register_custom_background() {
-	$args = array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	);
-
-	$args = apply_filters( 'nadine_custom_background_args', $args );
-
-	if ( function_exists( 'wp_get_theme' ) ) {
-		add_theme_support( 'custom-background', $args );
-	} else {
-		define( 'BACKGROUND_COLOR', $args['default-color'] );
-		if ( ! empty( $args['default-image'] ) )
-			define( 'BACKGROUND_IMAGE', $args['default-image'] );
-		add_custom_background();
+			wp_enqueue_script('jquery');
+			wp_enqueue_script('quicksand');
+			wp_enqueue_script('prettyPhoto');
+			wp_enqueue_script('easing');
+			wp_enqueue_script('custom');
+		}
 	}
-}
-add_action( 'after_setup_theme', 'nadine_register_custom_background' );
+	
+	add_action('init', 'register_js');
+	
+	// Register our Styles
+	function register_styles()
+	{
+		if (!is_admin()) {
+			wp_register_style('prettyPhoto', get_template_directory_uri() .	'/css/prettyPhoto.css');
+			wp_enqueue_style( 'prettyPhoto');
+		}		
+	}
+	
+	add_action('init', 'register_styles');
 
-/**
- * Register widgetized area and update sidebar with default widgets
- */
-function nadine_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'nadine' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-}
-add_action( 'widgets_init', 'nadine_widgets_init' );
-
-/**
- * Enqueue scripts and styles
- */
-function nadine_scripts() {
-	wp_enqueue_style( 'Nadine-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'Nadine-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'Nadine-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	//Setting Up Feature Images
+	if ( function_exists( 'add_theme_support' ) ) {
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 56, 56, true );
+		add_image_size( 'portfolio', 185, 185, true ); 
 	}
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'Nadine-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'nadine_scripts' );
+	// Include the portfolio functionality
+	include("portfolio/portfolio-post-type.php");
 
-/**
- * Implement the Custom Header feature
- */
-//require( get_template_directory() . '/inc/custom-header.php' );
+
+if (function_exists('add_theme_support')) {
+  add_theme_support('post-thumbnails');
+  set_post_thumbnail_size(185,185,true);
+}
+
+?>
